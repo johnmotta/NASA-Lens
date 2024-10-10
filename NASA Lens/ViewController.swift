74c278a22126fc3f24b2212bbae814dad6738f43
John Motta <johnmotta@MacBook-Pro-de-John.local>
2024-10-10 08:36:39 -0300

    first commit

---
+++ NASA Lens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NASA Lens
+//
+//  Created by John Motta on 10/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
