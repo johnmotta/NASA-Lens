a766d37a1a4b1fe354c784542cb14b2553325234
johnmotta <gabriel_motta31@hotmail.com>
2024-10-11 14:11:22 -0300

    Create AsteroidViewModel

--- NASA Lens/Asteroid/ViewModel/AsteroidViewModel.swift
+++ NASA Lens/Asteroid/ViewModel/AsteroidViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class AsteroidViewModel {
+    var asteroids: [String: [NearEarthObject]] = [:]
+        
+    func fetchData(startDate: String, endDate: String, completion: @escaping (Result<Void, NetworkError>) -> Void) {
+        ServiceManager.shared.fetchNEOData(startDate: startDate, endDate: endDate) { [weak self] result in
+            switch result {
+            case .success(let neoResponse):
+                self?.asteroids = neoResponse.nearEarthObjects
+                completion(.success(()))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func getAsteroidsSortedByDate() -> [(date: String, asteroids: [NearEarthObject])] {
+        let sorted = asteroids.sorted { $0.key < $1.key }.map { (date: $0.key, asteroids: $0.value) }
+        return sorted
+    }
+    
+    func getAsteroidCount() -> Int {
+        let count = asteroids.values.reduce(0) { $0 + $1.count }
+        return count
+    }
+}
