e59f49addfa90d77d9164559c0502166e0d4acd5
johnmotta <gabriel_motta31@hotmail.com>
2024-10-11 14:10:46 -0300

    Create AsteroidViewController

--- NASA Lens/Asteroid/View/AsteroidViewController.swift
+++ NASA Lens/Asteroid/View/AsteroidViewController.swift
@@ -5,4 +5,82 @@
 //  Created by John Motta on 11/10/24.
 //
 
-import Foundation
+import UIKit
+
+class AsteroidViewController: UIViewController {
+    private let viewModel = AsteroidViewModel()
+    private let tableView = UITableView()
+    private let activityIndicator = UIActivityIndicatorView(style: .large)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        fetchAsteroids()
+        navigationItem.title = "Asteroides próximos à Terra"
+    }
+
+    private func setupUI() {
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        view.addSubview(activityIndicator)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        tableView.register(AsteroidCell.self, forCellReuseIdentifier: "AsteroidCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    private func fetchAsteroids() {
+        activityIndicator.startAnimating()
+        viewModel.fetchData(startDate: "2023-05-01", endDate: "2023-05-07") { [weak self] result in
+            DispatchQueue.main.async {
+                self?.activityIndicator.stopAnimating()
+                switch result {
+                case .success:
+                    self?.tableView.reloadData()
+                case .failure(let error):
+                    self?.showError(error)
+                }
+            }
+        }
+    }
+    
+    private func showError(_ error: Error) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+}
+
+extension AsteroidViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.getAsteroidsSortedByDate().count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.getAsteroidsSortedByDate()[section].asteroids.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AsteroidCell", for: indexPath) as! AsteroidCell
+        let asteroid = viewModel.getAsteroidsSortedByDate()[indexPath.section].asteroids[indexPath.row]
+        cell.configure(with: asteroid)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return viewModel.getAsteroidsSortedByDate()[section].date
+    }
+}
