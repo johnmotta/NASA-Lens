4c3792c038a8db5fe176f47a05641e8a221668d5
johnmotta <gabriel_motta31@hotmail.com>
2024-10-17 09:47:14 -0300

    Added textcolor change

--- NASA Lens/Components/LabelDefault.swift
+++ NASA Lens/Components/LabelDefault.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class LabelDefault: UILabel {
     
-    init(fontSize: CGFloat, fontWeight: UIFont.Weight, opacity: CGFloat = 1, numberOfLines: Int = 0, textAligment: NSTextAlignment = .natural) {
+    init(textColor: UIColor = .label, fontSize: CGFloat, fontWeight: UIFont.Weight, opacity: CGFloat = 1, numberOfLines: Int = 0, textAligment: NSTextAlignment = .natural) {
         super.init(frame: .zero)
         
-        self.initDefault(fontSize, fontWeight, opacity, numberOfLines, textAligment)
+        self.initDefault(textColor, fontSize, fontWeight, opacity, numberOfLines, textAligment)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func initDefault(_ fontSize: CGFloat, _ fontWeight: UIFont.Weight,_ opacity: CGFloat,_ numberOfLines: Int, _ textAligment: NSTextAlignment) {
+    private func initDefault(_ textColor: UIColor, _ fontSize: CGFloat, _ fontWeight: UIFont.Weight,_ opacity: CGFloat,_ numberOfLines: Int, _ textAligment: NSTextAlignment) {
         self.translatesAutoresizingMaskIntoConstraints = false
+        self.textColor = textColor
         self.font = .systemFont(ofSize: fontSize, weight: fontWeight)
         self.textAlignment = textAligment
-        self.textColor = .label
         self.alpha = opacity
         self.numberOfLines = numberOfLines
     }
