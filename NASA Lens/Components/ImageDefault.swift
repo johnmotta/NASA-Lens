6ce44ab425cafae5fa6c692ddde81ff29f2d719b
johnmotta <gabriel_motta31@hotmail.com>
2024-10-17 09:36:24 -0300

    ClipsToBounds has been added

--- NASA Lens/Components/ImageDefault.swift
+++ NASA Lens/Components/ImageDefault.swift
@@ -9,18 +9,19 @@ import UIKit
 
 class ImageDefault: UIImageView {
     
-    init(contentMode: UIView.ContentMode) {
+    init(contentMode: UIView.ContentMode, clipsToBounds: Bool = false) {
         super.init(frame: .zero)
         
-        initDefault(contentMode)
+        initDefault(contentMode, clipsToBounds: clipsToBounds)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func initDefault(_ contentMode: UIView.ContentMode) {
+    private func initDefault(_ contentMode: UIView.ContentMode, clipsToBounds: Bool) {
         self.translatesAutoresizingMaskIntoConstraints = false
         self.contentMode = contentMode
+        self.clipsToBounds = clipsToBounds
     }
 }
